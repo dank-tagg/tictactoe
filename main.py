36be2b6d9f495cfeb137f77997433bd29092b5d8
dank-tagg <79766992+dank-tagg@users.noreply.github.com>
2022-10-13 10:02:30 +0200

    Final changes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bke import MLAgent, is_winner, start, load, opponent, RandomAgent, train_and_plot
 import random
 import os
-
 '''
 Wat zijn hyperparameters?
 
@@ -9,8 +8,9 @@ Een hyperparameter is een parameter waarvan de waarde de computer stuurt bij het
 De andere parameters worden van de training afgeleid.
 In deze programma zijn de hyperparameters alpha en epsilon:
     alpha: hoe hoger dit getal, hoe geneigd de agent zal zijn om nieuwe kennis te testen.
-    epsilon: hoe hoger dit getal, hoe vaker de agent een nieuwe zet probeert i.p.v. de beste zet.
+    epsilon: hoe hoger dit getal, hoe vaker de agent een nieuwe zet probeert i.p.v. de beste zet. Dus meer ruimte om nieuwe kennis te ontdekken.
 '''
+
 
 class MyAgent(MLAgent):
 
@@ -26,7 +26,12 @@ class MyAgent(MLAgent):
 
 random.seed(1)
 
-choices = ['Play against friend', 'Play against good bot', 'Play against noob bot', 'Train and validate bot']
+choices = [
+    'Play against friend',
+    'Play against good bot',
+    'Play against noob bot',
+    'Train and validate bot'
+]
 
 print('Play a game of tictactoe!')
 for i, choice in enumerate(choices):
@@ -39,7 +44,7 @@ def play(selection: int):
     os.system('clear')
     if selection == 0:
         # play against friend
-	    start()
+        start()
 
     elif selection == 1:
         # play against good bot
@@ -67,5 +72,6 @@ def play(selection: int):
     else:
         print('Not a valid choice, please check your answer and run again.')
         exit()
+
 
 play(int(ans))
